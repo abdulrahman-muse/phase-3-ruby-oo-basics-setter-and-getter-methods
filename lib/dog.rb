8883e21d6c23a9771ad6788f919add9d58066c50
abdulrahman muse <abdulrahmanmuse1@gmail.com>
2022-01-09 22:33:56 +0300

    commit tree a1286b093dcac351dd64472a9bd808eb1f024451

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
+def name=(dogsName)
+    @name = dogsName
 end
+def name
+    @name 
+end
+def breed=(dogsBreed)
+    @breed = dogsBreed
+end
+def breed
+    @breed 
+end
+end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-
-end
+    def name=(personsName)
+        @name = personsName
+    end
+    def name
+        @name 
+    end
+    def job=(personsJob)
+        @job = personsJob
+    end
+    def job
+        @job 
+    end
+    end
